4c8bf1d1658266b9a13517e7434cb7dae25d055c
Andreas Wehr <iam@grml.in>
2013-10-30 23:25:51 +0100

    extension using modules now

--- dist/gremlin.domelements.js
+++ dist/gremlin.domelements.js
@@ -1,43 +1,33 @@
 //@ sourceMappingURL=gremlin.domelements.map
 (function() {
-  var DomElements,
+  var addElements,
     __hasProp = {}.hasOwnProperty;
 
-  DomElements = (function() {
-    var addElements;
-
-    function DomElements() {}
-
-    addElements = function() {
-      var propertyName, selector, _ref, _results;
-      if (typeof this.klass.elements === 'object') {
-        _ref = this.klass.elements;
-        _results = [];
-        for (selector in _ref) {
-          if (!__hasProp.call(_ref, selector)) continue;
-          propertyName = _ref[selector];
-          if (!(typeof this[propertyName] === "undefined")) {
-            throw new TypeError("Element member " + propertyName + " already defined!");
-          }
-          if (!(typeof selector === "string")) {
-            throw new TypeError("Element selector have to be referenced by strings!");
-          }
-          _results.push(this[propertyName] = this.el.querySelectorAll(selector));
+  addElements = function() {
+    var propertyName, selector, _ref, _results;
+    if (typeof this.klass.elements === 'object') {
+      _ref = this.klass.elements;
+      _results = [];
+      for (selector in _ref) {
+        if (!__hasProp.call(_ref, selector)) continue;
+        propertyName = _ref[selector];
+        if (!(typeof this[propertyName] === "undefined")) {
+          throw new TypeError("Element member " + propertyName + " already defined!");
         }
-        return _results;
+        if (!(typeof selector === "string")) {
+          throw new TypeError("Element selector have to be referenced by strings!");
+        }
+        _results.push(this[propertyName] = this.el.querySelectorAll(selector));
       }
-    };
+      return _results;
+    }
+  };
 
-    DomElements.extend = function() {};
-
-    DomElements.bind = function(gremlinInstance) {
+  Gremlin.Module('domelements', {
+    extend: function() {},
+    bind: function(gremlinInstance) {
       return addElements.call(gremlinInstance);
-    };
-
-    return DomElements;
-
-  })();
-
-  Gremlin.registerExtension(DomElements);
+    }
+  });
 
 }).call(this);
--- src/domelements.coffee
+++ src/domelements.coffee
@@ -1,16 +1,13 @@
-class DomElements
-  addElements = () ->
-    if typeof @klass.elements is 'object'
-      for own selector, propertyName of @klass.elements
-        throw new TypeError "Element member #{propertyName} already defined!" unless (typeof @[propertyName] is "undefined")
-        throw new TypeError "Element selector have to be referenced by strings!" unless (typeof selector is "string")
-        @[propertyName] = @el.querySelectorAll(selector)
+addElements = () ->
+  if typeof @klass.elements is 'object'
+    for own selector, propertyName of @klass.elements
+      throw new TypeError "Element member #{propertyName} already defined!" unless (typeof @[propertyName] is "undefined")
+      throw new TypeError "Element selector have to be referenced by strings!" unless (typeof selector is "string")
+      @[propertyName] = @el.querySelectorAll(selector)
 
+Gremlin.Module 'domelements',
+  extend: ->
 
-  @extend: ->
-
-  @bind: (gremlinInstance) ->
-    addElements.call gremlinInstance
-
-Gremlin.registerExtension DomElements
+  bind: (gremlinInstance) ->
+      addElements.call gremlinInstance
  
